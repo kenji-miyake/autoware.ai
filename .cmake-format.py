008c4e42c25486ef1848ef0ee57609b0d89a0079
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2019-08-04 18:14:58 +0900

    Add .cmake-format.py

---
+++ .cmake-format.py
@@ -0,0 +1,29 @@
+# How wide to allow formatted cmake files
+line_width = 100
+
+# How many spaces to tab for indent
+tab_size = 2
+
+# If arglists are longer than this, break them always
+max_subargs_per_line = 1
+
+# If true, separate flow control names from their parentheses with a space
+separate_ctrl_name_with_space = False
+
+# If true, separate function names from parentheses with a space
+separate_fn_name_with_space = False
+
+# If a statement is wrapped to more than one line, than dangle the closing
+# parenthesis on it's own line
+dangle_parens = True
+
+# Format command names consistently as 'lower' or 'upper' case
+command_case = 'canonical'
+
+# Format keywords consistently as 'lower' or 'upper' case
+keyword_case = 'upper'
+
+# If a comment line starts with at least this many consecutive hash characters,
+# then don't lstrip() them off. This allows for lazy hash rulers where the first
+# hash char is not separated by space
+hashruler_min_length = 10
