5cd5036ca7fa924dfea2af76315d774ecd5c993e
Kenji Miyake <31987104+kenji-miyake@users.noreply.github.com>
2019-08-04 18:14:58 +0900

    Setup VSCode environment

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # autoware.ai
+
 Colcon Workspace for Autoware.AI
+
+## Usage
+
+```sh
+cd ~
+git clone https://github.com/kenji-miyake/autoware.ai.git
+cd autoware.ai
+mkdir -p src
+vcs import src < autoware.ai.repos
+python3 repos2workspace autoware.ai.repos # (Optional) Generate .code-workspace for VSCode
+code repos.code-workspace
+```
---
+++ repos2workspace.py
@@ -0,0 +1,51 @@
+import argparse
+import yaml
+import json
+import sys
+from pathlib import Path
+
+
+def repos2workspace(repos_file, workspace_file):
+    with open(repos_file, "r") as f:
+        repos = yaml.load(f, Loader=yaml.SafeLoader)
+
+    paths = [f"src/{path}" for path in repos["repositories"]]
+    folders = [{"path": "."}]
+
+    workspace = {
+        "folders": folders,
+        "settings": {
+            "files.watcherExclude": {
+                "**/.git/**": True,
+                "**/log/**": True,
+                "**/build/**": True,
+                "**/install/**": True
+            },
+            "scm.alwaysShowProviders": True,
+            "scm.alwaysShowActions": True,
+            "git.alwaysSignOff": True,
+            "git.ignoredRepositories": [str(repos_file.parent.absolute())],
+            "git.scanRepositories": paths,
+            "ros.distro": "melodic",
+            "python.autoComplete.extraPaths": [
+                "/opt/ros/melodic/lib/python2.7/dist-packages"
+            ],
+        }
+    }
+
+    with open(workspace_file, "w") as f:
+        json.dump(workspace, f, indent=2, sort_keys=False)
+
+
+def main(args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repos_file", type=Path)
+    parser.add_argument("--workspace-file", type=Path,
+                        default=Path("./autoware.ai.code-workspace"))
+    ns = parser.parse_args(args)
+
+    repos2workspace(ns.repos_file, ns.workspace_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
